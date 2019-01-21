007c3af00cd4c1281bba2fb3b25fbe2dba0d9afb
NamroN <normanargiolas@gmail.com>
2019-01-21 20:48:27 +0100

    add dependency

--- truffle-utils.js
+++ truffle-utils.js
@@ -1,3 +1,7 @@
+const Web3 = require('web3');
+const contract = require('truffle-contract');
+
+
 const config = require("./config");
 
 // console.log("----root ", config.root);
@@ -32,7 +36,7 @@ module.exports = {
                     content[network].contract_address = contract_address;
                     content[network].account_address = account_address;
                 } else { //add the network
-                        content[network] = data;
+                    content[network] = data;
                 }
                 fs.outputJsonSync(resolvePath, content);
 
@@ -43,5 +47,80 @@ module.exports = {
             console.log('error!');
             console.error(err);
         }
-    }
+    },
+
+    getWeb3: function (net) {
+
+        var web3Provider;
+        var web3 = null;
+
+
+        if (typeof net !== 'undefined') {
+
+            if (typeof net.provider !== 'undefined') {
+                //obtain credential by provider
+                web3Provider = net.provider();
+            } else {
+                //obtain credential by params
+
+                let url;
+                url = "http://" + net.host + ":" + net.port;
+                web3Provider = new Web3.providers.HttpProvider(url);
+            }
+
+            web3 = new Web3(web3Provider);
+            console.warn("Web3 Initialized!");
+
+        } else {
+            console.warn("ERROR with web3: No web3 detected. Impossible to create a New Instance");
+        }
+
+        this.web3 = web3;
+
+        return web3;
+    },
+
+    getAccounts: function () {
+        var self = this;
+
+
+        return new Promise(function (resolve, reject) {
+            self.web3.eth.getAccounts(function (e, accounts) {
+                if (e != null) {
+                    console.log("There was an error fetching your accounts.");
+                    reject(e);
+                } else {
+                    if (accounts.length == 0) {
+                        console.log("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+                        reject("No account fetch");
+                    }
+                    // self.accounts = accounts;       //Aggiunge al this accounts
+                    // account = self.accounts[0];
+                    // self.accountSMTP = self.accounts[0]; // //Aggiunge al this accountSMTP
+                    // console.log('Attempting to attach Sender contract from account ', accounts);
+
+                    resolve(accounts);
+                }
+            });
+        });
+    },
+
+    /**Use this to deploy a contract**/
+    getContractByArtifact: function (artifact, from) {
+        var Contract = contract(artifact);
+        Contract.setProvider(this.web3.currentProvider);
+        //fix for we web3 1.0
+        if (typeof Contract.currentProvider.sendAsync !== "function") {
+            Contract.currentProvider.sendAsync = function () {
+                return Contract.currentProvider.send.apply(
+                    Contract.currentProvider,
+                    arguments
+                );
+            };
+        }
+        Contract.defaults({from: from, gas: '30000000'});
+        return Contract;
+    },
+
 };
+
